c24cf6599f3eff5753499407afd0a8997a6a981f
Leônidas Luigy <52331780+leoLUIGY@users.noreply.github.com>
2025-09-23 22:52:24 -0300

    ajust tests (#6)
    * ajust tests
    
    * fix test

--- BookOrganizerTest/UnitTest1.cs
+++ BookOrganizerTest/UnitTest1.cs
@@ -1,5 +1,10 @@
 ﻿using BookOrganizer;
+using BookOrganizer.Controllers;
+using BookOrganizer.Data;
+using BookOrganizer.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 
@@ -12,14 +17,39 @@ namespace BookOrganizerTest
             var factory = new WebApplicationFactory<Program>();
             _factory = factory;
         }
-        //[Fact]
-        //public async Task TestTodoBookLoads()
-        //{
-        //    var client = _factory.CreateClient();
-        //    var response = await client.GetAsync("/TodoBook/Index");
-        //    int code = (int)response.StatusCode;
-        //    Assert.Equal(200, code);
-        //}
+
+        private BookOrganizerContext GetInMemoryContext()
+        {
+            var options = new DbContextOptionsBuilder<BookOrganizerContext>()
+                .UseSqlite("DataSource=:memory:")
+                .Options;
+
+            var context = new BookOrganizerContext(options);
+
+            context.Database.OpenConnection(); // abre a conexão in-memory
+            context.Database.EnsureCreated();  // cria as tabelas no SQLite
+
+            return context;
+        }
+        [Fact]
+        public async Task TestTodoBookIndex()
+        {
+            using var context = GetInMemoryContext();
+
+            // se precisar, insere dados fake no banco
+            context.Book.Add(new Book { Title = "Livro de teste" });
+            context.SaveChanges();
+
+            // usa o contexto nos testes
+            var controller = new TodoBookController(context);
+            var result = await controller.Index(null, null);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<BookGenreViewModel>(viewResult.Model);
+
+            Assert.Single(model.Books!);
+            Assert.Equal("Livro de teste", model.Books!.First().Title);
+        }
 
         [Theory]
         [InlineData("/")]
@@ -28,7 +58,7 @@ namespace BookOrganizerTest
         [InlineData("/Account/Login")]
         [InlineData("/Account/Register")]
 
-        public async void TestPagesWithoutId(string URL)
+        public async Task TestPagesWithoutId(string URL)
         {
             var client = _factory.CreateClient();
             var response = await client.GetAsync(URL);
